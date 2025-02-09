284f9e523b6b1fa5881dce1fef1f7c77c7885987
Anh Tuấn Trần <anhtuantran@Anhs-MacBook-Air.local>
2025-02-09 01:59:19 -0500

    update backend

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
-<<<<<<< HEAD
     
 
     try {
@@ -12,17 +11,6 @@ const userAuth = async (req, res, next) => {
         }
         else {
             const tokenDecode = jwt.verify(token, process.env.JWT_SECRECT);
-=======
-    const {token} = req.cookies;
-
-    if(!token){
-        return res.json({success: false, message: 'Not Authorized. Login Again'})
-    }
-
-    try {
-       
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRECT);
->>>>>>> fe80a82875357af125c7a5618d9fb4004f49245b
 
         if(tokenDecode.id){
             req.body.userId = tokenDecode.id
@@ -32,19 +20,12 @@ const userAuth = async (req, res, next) => {
 
         next();
 
-<<<<<<< HEAD
         }
         
 
-=======
->>>>>>> fe80a82875357af125c7a5618d9fb4004f49245b
     } catch (error) {
         res.json({success: false, message: error.message});
     }
 }
 
-<<<<<<< HEAD
 export default userAuth;
-=======
-export default userAuth;
->>>>>>> fe80a82875357af125c7a5618d9fb4004f49245b
--- server/server.js
+++ server/server.js
@@ -2,11 +2,9 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
-
 import connectDB from './config/mongodb.js'
 import authRouter from './routes/authRoutes.js'
 import userRouter from "./routes/userRoutes.js";
-<<<<<<< HEAD
 import modelRouter from "./routes/modelRoutes.js";
 import aiRouter from "./routes/aiRoutes.js";
 import OpenAI from "openai";
@@ -14,30 +12,20 @@ import dotenv from "dotenv";
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_SECRET,
 })
-=======
-
->>>>>>> fe80a82875357af125c7a5618d9fb4004f49245b
 const app = express();
 const port = process.emitWarning.PORT || 4000
 connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-<<<<<<< HEAD
 app.use(cors({origin: 'http://localhost:5174', credentials: true}))
-=======
-app.use(cors({credentials: true}))
->>>>>>> fe80a82875357af125c7a5618d9fb4004f49245b
 
 // API Endpoints
 app.get('/', (req, res)=> res.send("API Working"));
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
-<<<<<<< HEAD
 app.use('/api/ai', aiRouter)
 app.use('/api/model', modelRouter)
 
-=======
->>>>>>> fe80a82875357af125c7a5618d9fb4004f49245b
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
